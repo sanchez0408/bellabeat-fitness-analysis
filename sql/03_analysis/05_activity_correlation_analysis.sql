9097f8684cbafa1769200afe86171b7b9959f635
Sanchez guillaume <sanchez.guillaume84@gmail.com>
2026-06-10 23:47:57 +0200

    Update 05_activity_correlation_analysis.sql

--- sql/03_analysis/05_activity_correlation_analysis.sql
+++ sql/03_analysis/05_activity_correlation_analysis.sql
@@ -6,6 +6,6 @@ SELECT
   AVG(Calories) AS avg_calories,
   AVG(VeryActiveMinutes) AS avg_very_active,
   AVG(restless) AS avg_restless,
-  AVG(SedentaryMinutes) AS avg_sedentary,
+  AVG(SedentaryMinutes) AS avg_sedentary
 FROM `mon-projet-bigquery-481616.bellabeat_project_1.activity_and_sleep`
 GROUP BY Id
