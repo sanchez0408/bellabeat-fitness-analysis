2473926e202244bff17f6ff32d6f618b32450dd8
Sanchez guillaume <sanchez.guillaume84@gmail.com>
2026-06-10 10:01:39 +0200

    Update 03_sleep_time_cleaning.sql

--- sql/02_data_cleaning/03_sleep_time_cleaning.sql
+++ sql/02_data_cleaning/03_sleep_time_cleaning.sql
@@ -1,1 +1,33 @@
+-- Remove duplicates while keeping the highest value
+-- Keep track of 30 minutes or more of sleep recording
+-- Remove date of 11/03/2016
+
+CREATE OR REPLACE TABLE `mon-projet-bigquery-481616.bellabeat_project_1.sleep_time_cleaning` AS
+WITH duplicates AS (
+SELECT
+  Id,
+  Date,
+  MAX(asleep) AS asleep,
+  MAX(restless) AS restless,
+  MAX(awake) AS awake,
+  MAX(total_minutes_sleep) AS total_minutes_sleep
+FROM `mon-projet-bigquery-481616.bellabeat_project_1.sleep_time_cleaning`
+GROUP BY Id, Date
+),
+
+time_usage_valable AS (
+  SELECT *
+  FROM duplicates
+  WHERE total_minutes_sleep >= 30
+),
+
+days_of_analysis AS (
+  SELECT 
+    *
+  FROM time_usage_valable
+  WHERE Date != '2016-03-11'
+)
+
+SELECT *
+FROM days_of_analysis
 
