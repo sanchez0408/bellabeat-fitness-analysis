ea94ffad0c3f0d1bdaba8092575e84425df6a8a9
Sanchez guillaume <sanchez.guillaume84@gmail.com>
2026-06-10 09:57:18 +0200

    Create 00_daily_activity_cleaning.sql

---
+++ sql/02_data_cleaning/00_daily_activity_cleaning.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE TABLE `mon-projet-bigquery-481616.bellabeat_project_1.daily_activity_cleaning` AS
+/*WITH duplicates AS (*/
+SELECT
+  Id,
+  ActivityDate,
+  MAX(TotalSteps)AS TotalSteps,
+  MAX(TotalDistance) AS TotalDistance,
+  MAX(TrackerDistance) AS TrackerDistance,
+  MAX(LoggedActivitiesDistance)AS LoggedActivitiesDistance,
+  MAX(VeryActiveDistance)AS VeryActiveDistance,
+  MAX(ModeratelyActiveDistance)AS ModeratelyActiveDistance,
+  MAX(LightActiveDistance)AS LightActiveDistance,
+  MAX(SedentaryActiveDistance)AS SedentaryActiveDistance,
+  MAX(VeryActiveMinutes)AS VeryActiveMinutes,
+  MAX(FairlyActiveMinutes)AS FairlyActiveMinutes,
+  MAX(LightlyActiveMinutes)AS LightlyActiveMinutes,
+  MAX(SedentaryMinutes)AS SedentaryMinutes,
+  MAX(Calories) AS Calories
+FROM `mon-projet-bigquery-481616.bellabeat_project_1.daily_activity`
+GROUP BY Id, ActivityDate
